b4384eeac7d6ec4907afd5e2ec6ba9e6e80e67e3
Pierre Ynard <linkfanel@yahoo.fr>
2017-04-05 01:32:25 +0200

    Add two wideband WebSDR receivers in static list

--- static_rx.js
+++ static_rx.js
@@ -3,4 +3,16 @@
 // Licensed under GPLv3+
 
 var static_rx = [
+	{
+		name: '0-29 MHz WebSDR, University of Twente, Enschede, Netherlands',
+		url: 'http://websdr.ewi.utwente.nl:8901/',
+		gps: '(52.2381,6.8577)',
+		bands: '0-29160000',
+	},
+	{
+		name: '2-8 MHz WebSDR, Silec, Poland',
+		url: 'http://websdr.printf.cc:8901/',
+		gps: '(54.1605231, 21.5543704)',
+		bands: '2000000-8000000',
+	},
 ];
