47117bd38edbecf18bb8037fc50db8d26cb4a21d
Pierre Ynard <linkfanel@yahoo.fr>
2017-10-02 03:38:28 +0200

    Add now-usable antenna information to receivers in static list

--- static_rx.js
+++ static_rx.js
@@ -8,11 +8,13 @@ var static_rx = [
 		url: 'http://websdr.ewi.utwente.nl:8901/',
 		gps: '(52.2381,6.8577)',
 		bands: '0-29160000',
+		antenna: 'Mini-Whip',
 	},
 	{
 		name: '2-8 MHz WebSDR, Silec, Poland',
 		url: 'http://websdr.printf.cc:8901/',
 		gps: '(54.1605231, 21.5543704)',
 		bands: '2000000-8000000',
+		antenna: '30m random wire',
 	},
 ];
